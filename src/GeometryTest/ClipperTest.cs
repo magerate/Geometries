b545a4032f5e41f9ad2831451cd177f87e4cf9b9
magerate <magerate1129@gmail.com>
2015-11-12 09:28:17 +0800

    refactor test code

--- src/GeometryTest/ClipperTest.cs
+++ src/GeometryTest/ClipperTest.cs
@@ -10,7 +10,6 @@ namespace GeometryTest
 {
     //search \[.+\]\: \{(.+)\}
     //replace new Point $1,
-
     [TestFixture ()]
 	public class ClipperTest
 	{
@@ -23,38 +22,11 @@ namespace GeometryTest
 
             return pa;
         }
-        [Test]
-        public void Vertex_IntersectsTest()
-        {
-            Action action = () =>
-            {
-                var v1 = TestHelper.CreateRandomVertex();
-                var v2 = TestHelper.CreateRandomVertex();
-
-                var v3 = TestHelper.CreateRandomVertex();
-                var v4 = TestHelper.CreateRandomVertex();
-
-                double a = 0, b = 0;
-                if (Vertex.Intersects(v1, v2, v3, v4, ref a, ref b))
-                {
-                    //Assert.True(false);
-                    var cross = Segment.Intersect(v1.ToPoint(), v2.ToPoint(), v3.ToPoint(), v4.ToPoint());
-                    double aa = cross.Value.DistanceBetween(v1.ToPoint()) / v1.ToPoint().DistanceBetween(v2.ToPoint());
-
-                    double bb = cross.Value.DistanceBetween(v3.ToPoint()) / v3.ToPoint().DistanceBetween(v4.ToPoint());
-                    //Assert.AreEqual(a, aa);
-                    //Assert.AreEqual(b, bb);
-                    TestHelper.AlmostEqual(a, aa,1e-10);
-                    TestHelper.AlmostEqual(b, bb,1e-10);
-                }
-            };
-
-            action.RunBatch();
-        }
+       
 
      
         [Test]
-        public void Clipper_ClipTest1()
+        public void Clipper_IntersectTest1()
         {
             var p1 = new Point[]
             {
---
+++ src/GeometryTest/VertexTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+
+using Cession.Geometries;
+using Cession.Geometries.Clipping.GreinerHormann;
+
+namespace GeometryTest
+{
+	[TestFixture]
+	public class VertexTest
+	{
+        [Test]
+        public void Vertex_IntersectsTest()
+        {
+            Action action = () =>
+            {
+                var v1 = TestHelper.CreateRandomVertex();
+                var v2 = TestHelper.CreateRandomVertex();
+
+                var v3 = TestHelper.CreateRandomVertex();
+                var v4 = TestHelper.CreateRandomVertex();
+
+                double a = 0, b = 0;
+                if (Vertex.Intersects(v1, v2, v3, v4, ref a, ref b))
+                {
+                    //Assert.True(false);
+                    var cross = Segment.Intersect(v1.ToPoint(), v2.ToPoint(), v3.ToPoint(), v4.ToPoint());
+                    double aa = cross.Value.DistanceBetween(v1.ToPoint()) / v1.ToPoint().DistanceBetween(v2.ToPoint());
+
+                    double bb = cross.Value.DistanceBetween(v3.ToPoint()) / v3.ToPoint().DistanceBetween(v4.ToPoint());
+                    //Assert.AreEqual(a, aa);
+                    //Assert.AreEqual(b, bb);
+                    TestHelper.AlmostEqual(a, aa, 1e-10);
+                    TestHelper.AlmostEqual(b, bb, 1e-10);
+                }
+            };
+
+            action.RunBatch();
+        }
+    }
+}
+
